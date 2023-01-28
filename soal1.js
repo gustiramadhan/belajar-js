5fbd12fc9a3d5ef5e6800d6e824d54e72c6bfad4
GustiRamadhan <gustiramadhan57@gmail.com>
2023-01-29 04:16:01 +0700

    R2

--- soal1.js
+++ soal1.js
@@ -4,7 +4,7 @@ const angka = prompt('Input Bilangan: ');
 if(angka <= 0){
     console.log("Tidak bisa input bilangan negatif")
 } else if(angka % 2 == 0){
-    console.log("Akar pangkat 2 dari angka " +angka)
+    console.log("Akar pangkat 2 dari Bilangan " +angka)
     console.log("Adalah = " +Math.sqrt(angka));
 } else{
     console.log("Tidak bisa input bilangan ganjil")
