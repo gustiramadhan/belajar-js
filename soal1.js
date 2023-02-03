e57f76097d535426d77a763f0e41193dcf18c770
GustiRamadhan <gustiramadhan57@gmail.com>
2023-02-03 11:38:17 +0700

    R.3

--- soal1.js
+++ soal1.js
@@ -3,7 +3,7 @@ const angka = prompt('Input Bilangan: ');
 
 if(angka <= 0){
     console.log("Tidak bisa input bilangan negatif")
-} else if(angka % 2 == 0){
+} else if(angka %2 == 0){
     console.log("Akar pangkat 2 dari Bilangan " +angka)
     console.log("Adalah = " +Math.sqrt(angka));
 } else{
